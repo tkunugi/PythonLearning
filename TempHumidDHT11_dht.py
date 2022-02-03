a929b11c97ec266f6a824c3f664f3a26839e4974
tosgeo <>
2022-02-03 22:15:14 +0900

    comment corrected

--- TempHumidDHT11_dht.py
+++ TempHumidDHT11_dht.py
@@ -1,4 +1,3 @@
-#2022-2-3時点でinstance.read()ﾃで有効なデータが帰ってこない
 
 # from gpiozero import DigitalInputDevice
 import RPi.GPIO as GPIO
@@ -15,12 +14,12 @@ instance = dht11.DHT11(pin=14)
 try:
     while True:
         result = instance.read()
-        print(result, result.temperature)
+        #print(result, result.temperature)
         if result.is_valid():
-            # print("Last valid input: " + str(datetime.datetime.now()))
-            # print("Temperature: %-3.1f C" % result.temperature)
-            # print("Humidity: %-3.1f %%" % result.humidity)
-            print('OK')
+            print("Last valid input: " + str(datetime.datetime.now()))
+            print("Temperature: %-3.1f C" % result.temperature)
+            print("Humidity: %-3.1f %%" % result.humidity)
+            # print('OK')
        
        
 #　　if (not d0_input.value):
@@ -28,7 +27,7 @@ try:
 #　　else:
 #　　　print('Ground humidity: need')
        
-    time.sleep(1)   
+            time.sleep(1)   
 except KeyboardInterrupt:
     print("Cleanup")
     GPIO.cleanup()
