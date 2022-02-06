0b196b888831706ff174dec1ceaf7b3bf3acdaa0
tosgeo <>
2022-02-06 17:40:09 +0900

    extended to 3 colors

--- LEDTest.py
+++ LEDTest.py
@@ -1,11 +1,19 @@
 from gpiozero import LED, Button
 from time import sleep
 
+led_red = LED(23)
+led_green = LED(24)
+led_blue = LED(25)
 
-led_red = LED(20)
 
 while True:
     led_red.on()
     sleep(1)
     led_red.off()
+    led_green.on()
+    sleep(1)
+    led_green.off()
+    led_blue.on()
+    sleep(1)
+    led_blue.off()
     sleep(1)
