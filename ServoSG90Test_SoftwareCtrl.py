32e236a1cb1d68123b88e202604f79f77c8f0956
tosgeo <>
2023-02-06 07:07:54 +0900

    Changed GPIO port

--- ServoSG90Test_SoftwareCtrl.py
+++ ServoSG90Test_SoftwareCtrl.py
@@ -1,7 +1,7 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(5, min_pulse_width=0.5/1000, max_pulse_width=2.4/1000)
+servo = AngularServo(18, min_pulse_width=0.5/1000, max_pulse_width=2.4/1000)
 
 for i in list(range(0,-90, -1)):
     servo.angle = i
